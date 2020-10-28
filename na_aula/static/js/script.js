748d4b975539f1f73aab0602cd7bab593c74ff70
Kevin Zezel Gomes <kevin.zezel@hotmail.com>
2020-10-28 19:00:37 -0300

    Add files via upload

---
+++ na_aula/static/js/script.js
@@ -0,0 +1,40 @@
+$('#menu_consultar').on('click',function(){
+    $('#menu_consultar').addClass("active");
+    $('#menu_cadastrar').removeClass("active");
+    $('#menu_deletar').removeClass("active");
+    $('#menu_alterar').removeClass("active");
+    $('#consultar').css('display','');
+    $('#cadastrar').css('display','none');
+    $('#deletar').css('display','none');
+    $('#alterar').css('display','none');
+});
+$('#menu_cadastrar').on('click',function(){
+    $('#menu_cadastrar').addClass("active");
+    $('#menu_consultar').removeClass("active");
+    $('#menu_deletar').removeClass("active");
+    $('#menu_alterar').removeClass("active");
+    $('#consultar').css('display','none');
+    $('#cadastrar').css('display','');
+    $('#deletar').css('display','none');
+    $('#alterar').css('display','none');
+});
+$('#menu_alterar').on('click',function(){
+    $('#menu_cadastrar').removeClass("active");
+    $('#menu_consultar').removeClass("active");
+    $('#menu_deletar').removeClass("active");
+    $('#menu_alterar').addClass("active");
+    $('#consultar').css('display','none');
+    $('#cadastrar').css('display','none');
+    $('#deletar').css('display','none');
+    $('#alterar').css('display','');
+});
+$('#menu_deletar').on('click',function(){
+    $('#menu_cadastrar').removeClass("active");
+    $('#menu_consultar').removeClass("active");
+    $('#menu_deletar').addClass("active");
+    $('#menu_alterar').removeClass("active");
+    $('#consultar').css('display','none');
+    $('#cadastrar').css('display','none');
+    $('#deletar').css('display','');
+    $('#alterar').css('display','none');
+});
